ad32a65e19fb83665ce5d977e4b6bd5f02e13f46
kumarkonda <kumar.konda1983@gmail.com>
2015-05-20 18:09:49 +0530

    Created Links and Params

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -4,4 +4,19 @@ class DemoController < ApplicationController
 
   def index
   end
+
+  def hello
+    #render('index')
+    @array = [1,2,3,4,5]
+    @id = params['id']    
+    @page = params[:page].to_i
+  end
+
+  def other_hello
+    redirect_to(:controller => 'demo', :action => 'index')
+  end
+
+  def google
+    redirect_to("http://google.com")
+  end    
 end
